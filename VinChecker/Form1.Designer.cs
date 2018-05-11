f07685976fc3f195af3cd21d463375e49445206e
tmfahall <tmfahall@gmail.com>
2018-05-11 11:59:20 -0500

    first check in. functional but needs to be cleaned up

---
+++ VinChecker/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace VinChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Controls.Add(this.inputBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.searchButton, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(471, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputBox.Location = new System.Drawing.Point(73, 40);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(253, 20);
+            this.inputBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(332, 38);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(64, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Controls.Add(this.outputBox, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 100);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(471, 303);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputBox.Location = new System.Drawing.Point(50, 33);
+            this.outputBox.MaxLength = 9999999;
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(370, 236);
+            this.outputBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 403);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox outputBox;
+    }
+}
+
---
+++ VinChecker/Form1.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VinChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static HttpClient client = new HttpClient();
+
+        class ReturnData
+        {
+            public string Data { get; set; }
+        }
+
+        public class Result
+        {
+            public string Value { get; set; }
+            public string ValueId { get; set; }
+            public string Variable { get; set; }
+            public int VariableId { get; set; }
+        }
+
+        public class RootObject
+        {
+            public int Count { get; set; }
+            public string Message { get; set; }
+            public string SearchCriteria { get; set; }
+            public List<Result> Results { get; set; }
+        }
+
+        string outputString;
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(inputBox.Text, @"^[a-zA-Z0-9]+$") && inputBox.Text.Length >= 11 && inputBox.Text.Length <= 17)
+            {
+                string vin = inputBox.Text;
+                string url = string.Format("https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/", vin);
+
+                var responseText = "";
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(vin + "?format=json");
+                    response.EnsureSuccessStatusCode();
+                    responseText = await response.Content.ReadAsStringAsync();
+
+                    //JObject obj = JObject.Parse(responseText);
+                    //var token = (JArray)obj.SelectToken("Results");
+                    //var list = new List<RootObject>();
+
+                    //foreach (var item in token)
+                    //{
+                    //    string json = JsonConvert.SerializeObject(item.SelectToken("Results"));
+                    //    list.Add(JsonConvert.DeserializeObject<RootObject>(json));
+                    //}
+
+                    //responseText = "";
+
+                    //foreach (var item in list)
+                    //{
+                    //    responseText += item.Variable + " : " + item.Value + "\r\n";
+                    //}
+
+                    //outputBox.Text = list.GetType().ToString();
+
+                    string stringToExclude = "";
+                    JObject jo = JObject.Parse(responseText);
+
+                    JObject[] match = jo["Results"].Values<JObject>().Where(m => m["Value"].Value<string>() != stringToExclude).ToArray();
+
+                    var nullList = new List<string>();
+
+                    //left off here  ##Clean up the output #Remove VariableId and ValueId
+                    foreach (JProperty prop in match.Properties())
+                    {
+
+                        if (prop.Name == "Value")
+                        {
+                            outputString += "\r\n";
+                        }
+
+                        outputString += prop.Name + " : " + prop.Value + "\r\n";
+
+
+                        outputBox.Text = outputString;
+                    }
+                }
+                catch (Exception error)
+                {
+                    responseText = error.ToString();
+                    outputBox.Text = responseText;
+                }
+            }
+            else
+            {
+                outputBox.Text = "Error: VIN contains illegal characters, or is less than 11 or more than 17 characters long";
+            }
+        }
+    }
+}
